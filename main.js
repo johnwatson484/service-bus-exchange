41e47340623e84c381689cb253029f8c64b8ae2c
John Watson <johnwatson484@hotmail.com>
2022-02-06 22:04:30 +0000

    Refactored

--- main.js
+++ main.js
@@ -1,30 +1,54 @@
-const { app, BrowserWindow } = require('electron')
+// main.js
+
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, ipcMain, Notification } = require('electron')
 const path = require('path')
 
 const createWindow = () => {
+  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true
     }
   })
 
-  mainWindow.loadFile(path.join(__dirname, 'index.html'))
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
 }
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+ipcMain.handle('show-notification', (event, ...args) => {
+  const notification = {
+    title: 'New Task',
+    body: `Added: ${args[0]}`
+  }
+
+  new Notification(notification).show()
 })
